6f2d992fd4ee6594a0090aa18f187779f8377c91
YuuSoeno <yuuyuu8312@yahoo.co.jp>
2018-07-03 22:10:14 +0900

    7月前半課題”文字列での計算”
    一部変数名の変更（fifthNo → addendNo）

--- quinarynumber/main/QuinaryNumber.java
+++ quinarynumber/main/QuinaryNumber.java
@@ -10,18 +10,19 @@ import logic.ConvertStr;
  * @version 2018/7/2
  * */
 public class QuinaryNumber {
-	/** 5進数変換用変数*/
+	/** 5進数変換用変数 */
 	public static final int quinaryNum = 5;
 
 	public static void main(String[] args) {
-		/** 入力受け取り：文字列（5進数） */
+		/** 入力受け取り：文字列（A～E） */
 		String inputAddend1 = null;
 		String inputAddend2 = null;
-		/** 5進数（String型で格納） */
-		String fifthNo1;
-		String fifthNo2;
+		/** 文字列⇒5進数 格納用変数（String型で格納） */
+		String addendNo1;
+		String addendNo2;
 		/** 変換処理用 */
 		ConvertStr fc = new ConvertStr();
+
 		Scanner sc = new Scanner(System.in);
 
 		// 入力された文字を受け取る。
@@ -34,16 +35,16 @@ public class QuinaryNumber {
 		// 入力が正しいか、ConvertStrクラスのinputCheck()メソッドを使って確認する
 		if (fc.inputCheck(inputAddend1) && fc.inputCheck(inputAddend2)) {
 			// 受け取った2つの文字列を5進数に変換
-			fifthNo1 = fc.inputConvertQuinary(inputAddend1);
-			fifthNo2 = fc.inputConvertQuinary(inputAddend2);
+			addendNo1 = fc.inputConvertQuinary(inputAddend1);
+			addendNo2 = fc.inputConvertQuinary(inputAddend2);
 		} else {
 			// 入力が不正の場合
 			return;
 		}
 
 		// 5進数⇒10進数に変換し、合計を格納する
-		int amountNum = Integer.parseInt(fifthNo1, quinaryNum)
-				+ Integer.parseInt(fifthNo2, quinaryNum);
+		int amountNum = Integer.parseInt(addendNo1, quinaryNum)
+				+ Integer.parseInt(addendNo2, quinaryNum);
 		// 10進数での合計を5進数に変換
 		String amountFifthNum = Integer.toString(amountNum, quinaryNum);
 
