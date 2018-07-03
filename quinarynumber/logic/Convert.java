35c42d624f002355726c817c2530ba8d8616245e
YuuSoeno <yuuyuu8312@yahoo.co.jp>
2018-07-03 22:29:51 +0900

    7月前半課題”文字列での計算”
    Convertクラスにjavadoc追加

--- quinarynumber/logic/Convert.java
+++ quinarynumber/logic/Convert.java
@@ -1,6 +1,11 @@
 package logic;
 
-public class ConvertStr {
+/**
+*
+* @author yuu.s
+* @version 2018/7/2
+* */
+public class Convert {
 	/** 5進数変換チェック用配列 */
 	public static final String[] quinaryWords = { "A", "B", "C", "D", "E" };
 
